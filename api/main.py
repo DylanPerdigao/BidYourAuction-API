e014f90c1ff05c45a81e7901dc8b99e5968b9f8a
Dylan Perdigão <32035460+DylanPerdigao@users.noreply.github.com>
2021-07-07 19:00:14 +0100

    Update main.py

--- api/main.py
+++ api/main.py
@@ -428,8 +428,5 @@ if __name__ == '__main__':
         port=BIDYOURAUCTION_PORT,
         database=BIDYOURAUCTION_DB
     )
-    try:
-        app.run(debug=True, host='localhost', port=8080)
-    except:
-        app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
         
